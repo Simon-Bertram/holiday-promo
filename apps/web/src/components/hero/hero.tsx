0786e0ea42e40eac93d01595cbb136cc7aacc47f
Simon-Bertram <simonbertram@hotmail.com>
2025-11-03 15:31:14 +0000

    More attempt at fixing hero image/text

--- apps/web/src/components/hero/hero.tsx
+++ apps/web/src/components/hero/hero.tsx
@@ -26,8 +26,8 @@ export default function Hero() {
         sizes="(min-width: 768px) 100vw, 100vw"
         src="https://res.cloudinary.com/dulwhlyqt/video/upload/f_jpg,q_auto,dpr_auto,c_fill,g_auto,w_1920,h_1080,so_1/7677655-hd_1920_1080_25fps_altt0g.jpg"
       />
-      <div className="z-10 col-start-2 col-end-12 row-start-5 row-end-8 flex flex-col rounded-lg bg-black/70 p-6 text-white md:col-start-8 md:col-end-13 md:row-start-7 md:row-end-9">
-        <h1 className="mb-4 font-black text-4xl">
+      <div className="z-10 col-start-2 col-end-12 row-start-5 row-end-8 flex flex-col rounded-lg bg-black/70 p-6 text-white md:col-start-8 md:col-end-13 md:row-start-6 md:row-end-8">
+        <h1 className="mb-4 font-black text-3xl md:text-5xl">
           Unlock your best holiday yet
         </h1>
         <p className="mb-4 text-lg">
